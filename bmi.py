790df8369573334731ff54ab912d6ebe88215e59
Farhan <farhan.24@ichat.sp.edu.sg>
2025-11-09 18:46:19 +0800

    changes to make test work

--- bmi.py
+++ bmi.py
@@ -7,12 +7,11 @@ def calculate_bmi(height, weight):
 
     if (bmi>25.0):
         classification = -1
-    elif (18.5 < bmi < 25.0):
+    elif (18.5 <= bmi <= 25.0):
         classification = 0
     elif (bmi < 18.5):
         classification = 1
 
     return bmi, classification
 
-calculate_bmi(weight=57, height=1.73)
 
